9d4351e1c19bf4dd60776cb8689c03ae6deaf8f7
ndwork <ndwork@Nicholass-MacBook-Pro-5.local>
2017-02-04 11:07:53 -0800

    Code used to generate results for ISMRM abstract.

--- epgInversionFSE.m
+++ epgInversionFSE.m
@@ -1,103 +1,95 @@
 
-function alpha = epgInversionFSE( t1, t2, T1, T2 )
+function alphaOut = epgInversionFSE( t1, t2, T1, T2, J, alpha, Q0 )
+  % J is the number of echoes in FSE sequence
 
-  J = 1;  % number of echoes in FSE sequence
-  F0D = 0.1;   % Desired signal strength at all echoes
-  theta = 30 * pi/180;  % max value of alpha
-  N = 4;  % largest EGP coefficient to store
-  phi = 0;
-
-  alpha0 = findAlpha0( F0D, theta );
-  Ralpha0 = epgMakeR( alpha0, phi-pi/2 );  % Meet CPMG conditions
-  Q0m = sparse(3,N);  Q0m(3,1)=1;
-	Q0 = sparse( Ralpha0 * Q0m );
+  F0mD = 1.0;   % Desired signal strength at all echoes
+  N = 100;  % largest EGP coefficient to store
+  %theta = 30 * pi/180;  % max value of alpha
+  theta = pi/2;
 
   E11 = exp( -t1 / T1 );
+  E12 = exp( -t1 / T2 );
+  E21 = exp( -t2 / T1 );
   E22 = exp( -t2 / T2 );
-  E1122 = E11 * E22;
-  E1 = exp( -(t1+t2)/T1 );
-  E2 = exp( -(t1+t2)/T2 );
+  
+  Ra = epgMakeR( alpha, pi/2 );  % phi0=0, phi=pi/2
 
-  %b = makebVec( t1, t2, T1, T2, J, Q0, F0D );
-  %H = makeH( t1, t2, T1, T2, J, Q0, phi );
+  A = makeA( E11, E12, E21, E22, size(Q0,2), Ra );  
+  B = makeB( E11, E12, E21, E22, size(Q0,2), Ra );
+
+  Qtilde = sparse( size(Q0,1), size(Q0,2) );
+  Qtilde(1,2) = E22*(1-E11)*sin(alpha);
 
   megaH = zeros(2*J,J);
   megab = zeros(2*J,1);
   for thisJ=1:J
-    b = makebVec( E11, E2, thisJ, Q0, F0D );
+    disp([ 'Working on echo ', num2str(thisJ), ' of ', num2str(J) ]);
+    b = makebVec( A, B, thisJ, Q0, Qtilde, alpha, F0mD );
     megab(1+(thisJ-1)*2:2+(thisJ-1)*2) = b;
-    H = makeH( E11, E22, E1122, E1, E2, thisJ, Q0, phi );
-    megaH(1+(thisJ-1)*2:2+(thisJ-1)*2) = H;
+    H = makeH( A, B, thisJ, Q0, Qtilde );
+    megaH(1+(thisJ-1)*2:2+(thisJ-1)*2,1:size(H,2)) = [ H(1,:); H(numel(Q0)+1,:); ];
+  end
+  megab = real( megab );
+  megaH = real( megaH );
+
+  useLsqLin = 1;
+  if useLsqLin == 1,
+    lb = -theta * ones( J, 1 );
+    ub = theta * ones( J, 1 );
+    dAlpha = lsqlin( megaH, megab, [], [], [], [], lb, ub );
+  else
+    opts = tfocs;
+    opts.alg = 'N83';
+    opts.maxIts = 200;
+    opts.printEvery = 1;
+    opts.tol = 1d-8;
+    x0 = zeros( J, 1 );
+    [dAlpha,tfocsOptOut] = tfocs( smooth_quad, { megaH, -megab }, ...
+      proj_linf(theta), x0, opts );
+    %[dAlpha,tfocsOptOut] = tfocs( smooth_huber(0.1), { megaH, -megab }, ...
+    %  proj_linf(theta), x0, opts );
   end
 
-  alpha = megaH \ megab;
+  alphaOut = alpha + dAlpha;
 end
 
 
-function alpha0 = findAlpha0( F0d, theta, Qin )
-  if nargin < 3, Qin=[0;0;1;]; end;
+function bVec = makebVec( A, B, M, Q0, Qtilde, alpha, F0D )
+  Q0_vec = [ real(Q0(:)); imag(Q0(:)); ];
+  Qtilde_vec = [ real(Qtilde(:)); imag(Qtilde(:)); ];
 
-  dAlpha = 1/360*pi;
-  bestAlpha0 = 0;
-  cost = 99999;
-  for alpha = -theta:dAlpha:theta
-    Ralpha0 = epgMakeR( alpha, -pi/2 );
-    thisQ = Ralpha0 * Qin;
-    if abs(thisQ(1,1)-F0d) < cost
-      bestAlpha0 = alpha;
-      cost = abs(thisQ(1,1)-F0d);
+  tmp = A^M * Q0_vec;
+  for i=1:M
+    tmp = tmp - A^(M-i)*B*A^i * Q0_vec * alpha;
+  end
+
+  for j=1:M
+    tmp = tmp + A^(M-j) * Qtilde_vec;
+
+    for i=j+1:M
+      tmp = tmp - A^(M-i) * B * A^i * Qtilde_vec;
     end
   end
-  alpha0 = bestAlpha0;
+
+  bVec = [ 0; F0D;] - [ tmp(1,1); tmp(numel(Q0)+1,1); ];
 end
 
 
-function b = makebVec( E11, E2, M, Q0, F0D )
-  QE = sparse( size(Q0,1), size(Q0,2) ); QE(3,1) = 1;
-
-  A = makeA( E11, E2, size(Q0,2) );
-
-  Q0_vec = [ real(Q0(:)); imag(Q0(:)); ];
-  QE_vec = [ real(QE(:)); imag(QE(:)); ];
-  tmp = A^M * Q0_vec;
-  for i=2:M
-    tmp = tmp + A^(M-i+1) * QE_vec;
-  end
-
-  b = [ F0D; 0;] - [ tmp(1,1); tmp(numel(Q0)+1,1); ];
-end
-
-
-function H = makeH( E11, E22, E1122, E1, E2, M, Q0, phi )
-  % t1 is the time until the RF pulse
-  % t2 is the time from the RF pulse to the end of the repetition
-  % T1 and T2 are the decay constants
+function H = makeH( A, B, M, Q0, Qtilde )
   % M is the number of echoes
   % Q0 is the initial state
-  % phi is the phase of the RF pulse
-
-  A = makeA( E11, E2, size(Q0,2) );
-  B = makeB( E1122, E1, size(Q0,2) );
-
-  QE = sparse( size(Q0,1), size(Q0,2) );  QE(3,1) = 1;
-  QR = sparse( size(Q0,1), size(Q0,2) );  QR(1,2) = E22*(1-E11)*(-1i*exp(1i*phi));
 
   preH = cell(M,M);
-  Q0_vec = [ real(Q0(:)); imag(Q0(:)); ];  
+  Q0_vec = [ real(Q0(:)); imag(Q0(:)); ];
   for i=1:M
     preH{1,i} = A^(M-i) * B * A^(i-1) * Q0_vec;
   end
 
-  QE_vec = [ real(QE(:)); imag(QE(:)); ];
-  for j=1:M
+  Qtilde_vec = [ real(Qtilde(:)); imag(Qtilde(:)); ];
+  for j=2:M
     for i=j:M
-      preH{j,i} = A^(M-i) * B * A^(i-j) * QE_vec;
+      preH{j,i} = A^(M-i) * B * A^(i-j) * Qtilde_vec;
     end
-  end
-
-  QR_vec = [ real(QR(:)); imag(QR(:)); ];
-  for i=2:M
-    preH{i,i} = preH{i,i} + A^(M-i+1) * QR_vec;
   end
 
   H = cell(1,M);
@@ -109,78 +101,132 @@ function H = makeH( E11, E22, E1122, E1, E2, M, Q0, phi )
   end
 
   H = cell2mat(H);
-  H = [ H(1:M,:); H(numel(Q0)+1,:); ];
 end
 
 
-function A = makeA( E11, E2, N )
+function A = makeA( E11, E12, E21, E22, N, Ra )
   % N is the index of the largest EPG coefficient vector retained
-  AFn_real = [ circshift( E2*eye(N), 2, 2 ), zeros(N) ];
-  AFn_real(end-1:end,:)=0;
-  AFn_imag = [ zeros(N), circshift( E2*eye(N), 2, 2 ) ];
-  AFn_imag(end-1:end,:) = 0;
 
-  AFp_real = [ circshift( E2*eye(N), -2, 2 ), zeros(N) ];
-  AFp_real(1,:)=0;  AFn_real(1,3)= E2;  % AFn here is not a mistake
-  AFp_real(2,:)=0;  AFn_real(2,2)= E2;
-  AFp_imag = [ zeros(N), circshift( E2*eye(N), -2, 2 ) ];
-  AFp_imag(1,:)=0;  AFp_imag(1,N+3)= -E2;
-  AFp_imag(2,:)=0;  AFp_imag(2,N+2)= -E2;
+  % Notation for matrices: A<out><in>
+  AFpFn_real = E22 * Ra(1,2) * E12 * [ eye(N), zeros(N) ];
+  AFpFn_imag = E22 * Ra(1,2) * E12 * [ zeros(N), eye(N) ];
 
-  AZ = [ E11*eye(N), zeros(N); zeros(N), E11*eye(N) ];
+  AFpFp_real = E22 * Ra(1,1) * E12 * [ circshift(eye(N),-2,2), zeros(N) ];
+  AFpFp_real(1:2,:)=0;
+  AFpFp_imag = E22 * Ra(1,1) * E12 * [ zeros(N), circshift(eye(N),-2,2) ];
+  AFpFp_imag(1:2,:)=0;
+
+  AFpFn_real(1,3) = AFpFn_real(1,3) + E22 * Ra(1,1) * E12;
+  AFpFn_real(2,2) = AFpFn_real(2,2) + E22 * Ra(1,1) * E12;
+  AFpFn_imag(1,3) = AFpFn_imag(1,3) - E22 * Ra(1,1) * E12;
+  AFpFn_imag(2,2) = AFpFn_imag(2,2) - E22 * Ra(1,1) * E12;
+
+  AFpZ_real = E22 * Ra(1,3) * E11 * [ circshift(eye(N),-1,2), zeros(N) ];
+  AFpZ_real(1,:)=0;  AFpZ_real(1,2)=1;
+  AFpZ_imag = E22 * Ra(1,3) * E11 * [ zeros(N), circshift(eye(N),-1,2) ];
+  AFpZ_imag(1,:)=0;  AFpZ_imag(1,2)= -1;
+
+  AFnFp_real = E22 * Ra(2,1) * E12 * [ eye(N), zeros(N) ];
+  AFnFp_imag = E22 * Ra(2,1) * E12 * [ zeros(N), eye(N) ];
+
+  AFnFn_real = E22 * Ra(2,2) * E12 * [ circshift(eye(N),2,2), zeros(N) ];
+  AFnFn_real(end-1:end,:)=0;
+  AFnFn_imag = E22 * Ra(2,2) * E12 * [ zeros(N), circshift(eye(N),2,2) ];
+  AFnFn_imag(end-1:end,:)=0;
+
+  AFnZ_real = E22 * Ra(2,3) * E11 * [ circshift(eye(N),1,2), zeros(N) ];
+  AFnZ_real(end,:)=0;
+  AFnZ_imag = E22 * Ra(2,3) * E11 * [ zeros(N), circshift(eye(N),1,2) ];
+  AFnZ_imag(end,:)=0;
+
+  AZFn_real = E21 * Ra(3,2) * E12 * [ circshift(eye(N),1,2), zeros(N) ];
+  AZFn_real(end,:)=0;
+  AZFn_imag = E21 * Ra(3,2) * E12 * [ zeros(N), circshift(eye(N),1,2) ];
+  AZFn_imag(end,:)=0;
+
+  AZFp_real = E21 * Ra(3,1) * E12 * [ circshift(eye(N),-1,2), zeros(N) ];
+  AZFp_real(1,:)=0;
+  AZFp_imag = E21 * Ra(3,1) * E12 * [ zeros(N), circshift(eye(N),-1,2) ];
+  AZFp_imag(1,:)=0;
+
+  AZFn_real(1,2) = AZFn_real(1,2) + E21 * Ra(3,1) * E12;
+  AZFn_imag(1,2) = AZFn_imag(1,2) - E21 * Ra(3,1) * E12;
+
+  AZZ_real = E21 * Ra(3,3) * E11 * [ eye(N), zeros(N) ];
+  AZZ_imag = E21 * Ra(3,3) * E11 * [ zeros(N), eye(N) ];
 
   A = sparse( 2*3*N, 2*3*N );
-  A(1:3:end,1:3:end) = [ AFp_real; AFp_imag; ];
-  A(2:3:end,2:3:end) = [ AFn_real; AFn_imag; ];
-  A(3:3:end,3:3:end) = AZ;
+  A(1:3:end,1:3:end) = A(1:3:end,1:3:end) + [ AFpFp_real; AFpFp_imag; ];
+  A(1:3:end,2:3:end) = A(1:3:end,2:3:end) + [ AFpFn_real; AFpFn_imag; ];
+  A(1:3:end,3:3:end) = A(1:3:end,3:3:end) + [ AFpZ_real; AFpZ_imag; ];
+  A(2:3:end,1:3:end) = A(2:3:end,1:3:end) + [ AFnFp_real; AFnFp_imag; ];
+  A(2:3:end,2:3:end) = A(2:3:end,2:3:end) + [ AFnFn_real; AFnFn_imag; ];
+  A(2:3:end,3:3:end) = A(2:3:end,3:3:end) + [ AFnZ_real; AFnZ_imag; ];
+  A(3:3:end,1:3:end) = A(3:3:end,1:3:end) + [ AZFp_real; AZFp_imag; ];
+  A(3:3:end,2:3:end) = A(3:3:end,2:3:end) + [ AZFn_real; AZFn_imag; ];
+  A(3:3:end,3:3:end) = A(3:3:end,3:3:end) + [ AZZ_real; AZZ_imag; ];
 end
 
 
-function B = makeB( E1122, E1, N )
+function B = makeB( E11, E12, E21, E22, N, Ra )
   % N is the index of the largest EPG coefficient vector retained
-  BFn_real = [ circshift( E1122*eye(N), 1, 2 ), zeros(N) ];
-  BFn_real(end,:)=0;
-  BFn_imag = [ zeros(N), circshift( E1122*eye(N), 1, 2 ) ];
-  BFn_imag(end,:)=0;
 
-  BFp_real = [ circshift( -E1122*eye(N), -1, 2 ), zeros(N) ];
-  BFp_real(1,:)=0;  BFp_real(1,2) = -E1122;
-  BFp_imag = [ zeros(N), circshift( -E1122*eye(N), -1, 2 ) ];
-  BFp_imag(1,:)=0;  BFp_imag(1,N+2)= E1122;
+  % Notation for matrices: B<out><in>
+  BFpFn_real = E22 * Ra(1,3) * -E12/2 * [ eye(N), zeros(N) ];
+  BFpFn_imag = E22 * Ra(1,3) * -E12/2 * [ zeros(N), eye(N) ];
 
-  BZm_real = [ circshift( E1*eye(N), 1, 2 ), zeros(N) ];
-  BZm_real(end,:)=0;
-  BZm_imag = [ zeros(N), circshift( E1*eye(N), 1, 2 ) ];
-  BZm_imag(end,:)=0;
+  BFpFp_real = E22 * Ra(1,3) * -E12/2 * [ circshift(eye(N),-2,2), zeros(N) ];
+  BFpFp_real(1:2,:)=0;
+  BFpFp_imag = E22 * Ra(1,3) * -E12/2 * [ zeros(N), circshift(eye(N),-2,2) ];
+  BFpFp_imag(1:2,:)=0;
+  BFpFn_real(1,3) = BFpFn_real(1,3) + E22 * Ra(1,3) * -E12/2;
+  BFpFn_imag(1,3) = BFpFn_imag(1,3) - E22 * Ra(1,3) * -E12/2;
+  BFpFn_real(2,2) = BFpFn_real(2,2) + E22 * Ra(1,3) * -E12/2;
+  BFpFn_imag(2,2) = BFpFn_imag(2,2) - E22 * Ra(1,3) * -E12/2;
 
-  BZp_real = [ circshift( -E1*eye(N), -1, 2 ), zeros(N) ];
-  BZp_real(1,:)=0;  BZm_real(1,2) = -E1;  % BZm here is not a mistake
-  BZp_imag = [ zeros(N), circshift( -E1*eye(N), -1, 2 ) ];
-  BZp_imag(1,:)=0;  BZm_imag(2,N+2) = E1;  % BZm here is not a mistake
+  BFpZ_real = E22 * E11 * ( Ra(1,1) + Ra(1,2) ) *  [ circshift(eye(N),-1,2), zeros(N) ];
+  BFpZ_real(1,:)=0;  BFpZ_real(1,2) = E22 * E11 * ( Ra(1,1) + Ra(1,2) );
+  BFpZ_imag = E22 * E11 * ( Ra(1,1) + Ra(1,2) ) * [ zeros(N), circshift(eye(N),-1,2) ];
+  BFpZ_imag(1,:)=0;  BFpZ_imag(1,2) = -E22 * E11 * ( Ra(1,1) + Ra(1,2) );
+
+  BFnFp_real = E22 * Ra(2,3) * -E12/2 * [ eye(N), zeros(N) ];
+  BFnFp_imag = E22 * Ra(2,3) * -E12/2 * [ zeros(N), eye(N) ];
+
+  BFnFn_real = E22 * Ra(2,3) * -E12 * [ circshift(eye(N),2,2), zeros(N) ];
+  BFnFn_real(end-1:end,:) = 0;
+  BFnFn_imag = E22 * Ra(2,3) * -E12 * [ zeros(N), circshift(eye(N),2,2) ];
+  BFnFn_imag(end-1:end,:) = 0;
+
+  BFnZ_real = E22 * E11 * ( Ra(2,1) + Ra(2,2) ) * [ circshift(eye(N),1,2), zeros(N) ];
+  BFnZ_real(end,:) = 0;
+  BFnZ_imag = E22 * E11 * ( Ra(2,1) + Ra(2,2) ) * [ zeros(N), circshift(eye(N),1,2) ];
+  BFnZ_imag(end,:) = 0;
+
+  BZFn_real = E21 * Ra(3,3) * -E12/2 * [ circshift(eye(N),1,2), zeros(N) ];
+  BZFn_real(end,:) = 0;
+  BZFn_imag = E21 * Ra(3,3) * -E12/2 * [ zeros(N), circshift(eye(N),1,2) ];
+  BZFn_imag(end,:) = 0;
+
+  BZFp_real = E21 * Ra(3,3) * -E12/2 * [ circshift(eye(N),-1,2), eye(N) ];
+  BZFp_real(1,:) = 0;
+  BZFp_imag = E21 * Ra(3,3) * -E12/2 * [ eye(N), circshift(eye(N),-1,2) ];
+  BZFp_imag(1,:) = 0;
+
+  BZFn_real(1,2) = BZFn_real(1,2) + E21 * Ra(3,3) * -E12/2;
+  BZFn_imag(1,2) = BZFn_imag(1,2) - E21 * Ra(3,3) * -E12/2;
+
+  BZZ_real = E21 * E11 * ( Ra(3,1) + Ra(3,2) ) * [ eye(N), zeros(N) ];
+  BZZ_imag = E21 * E11 * ( Ra(3,1) + Ra(3,2) ) * [ zeros(N), eye(N) ];
 
   B = sparse( 2*3*N, 2*3*N );
-  BFp = [ BFp_real; BFp_imag; ];  B(1:3:end,3:3:end) = BFp;
-  BFn = [ BFn_real; BFn_imag; ];  B(2:3:end,3:3:end) = BFn;
-  BZ1 = [ BZm_real; BZm_imag; ];  B(3:3:end,2:3:end) = BZ1;
-  BZ2 = [ BZp_real; BZp_imag; ];  B(3:3:end,1:3:end) = BZ2;
-end
-
-
-function AQ = applyA( Q, E11, E2 )
-  AQ = E11 * Q;  % only last row is correct here
-  AQ(2,1:end-2) = E2 * Q(2,3:end);
-  AQ(2,end-2:end) = 0;
-  AQ(1,3:end) = E2 * Q(1,1:end-2);
-  AQ(1,2) = E2 * conj( Q(2,2) );
-  AQ(1,1) = E2 * conj( Q(1,1) );
-end
-
-function BQ = applyB( Q, E1122, E1 )
-  BQ = sparse( size(Q,1), size(Q,2) );
-  BQ(3,2:end-1) = E1 * ( Q(2,3:end) - Q(1,1:end-2) );
-  BQ(3,1) = E1 * ( Q(2,2) - conj( Q(1,2) ) );
-  BQ(2,1:end-1) = E1122 * Q(3,2:end);
-  BQ(1,2:end) = -E1122 * Q(3,1:end-1);
-  BQ(1,1) = -E1122 * conj( Q(3,2) );
+  B(1:3:end,1:3:end) = B(1:3:end,1:3:end) + [ BFpFp_real; BFpFp_imag; ];
+  B(1:3:end,2:3:end) = B(1:3:end,2:3:end) + [ BFpFn_real; BFpFn_imag; ];
+  B(1:3:end,3:3:end) = B(1:3:end,3:3:end) + [ BFpZ_real; BFpZ_imag; ];
+  B(2:3:end,1:3:end) = B(2:3:end,1:3:end) + [ BFnFp_real; BFnFp_imag; ];
+  B(2:3:end,2:3:end) = B(2:3:end,2:3:end) + [ BFnFn_real; BFnFn_imag; ];
+  B(2:3:end,2:3:end) = B(2:3:end,2:3:end) + [ BFnZ_real; BFnZ_imag; ];
+  B(3:3:end,1:3:end) = B(3:3:end,1:3:end) + [ BZFp_real; BZFp_imag; ];
+  B(3:3:end,2:3:end) = B(3:3:end,2:3:end) + [ BZFn_real; BZFn_imag; ];
+  B(3:3:end,3:3:end) = B(3:3:end,3:3:end) + [ BZZ_real; BZZ_imag; ];
 end
 
--- run_epgInversionFSE.m
+++ run_epgInversionFSE.m
@@ -2,14 +2,64 @@
 function run_epgInversionFSE
   close all; clear;
 
-  t1 = 100;
-  t2 = 115;
-
+  J = 50;  % The number of echoes
+  t1 = 10;
+  t2 = 10;
   T1 = 1000;
-  T2 = 800;
+  T2 = 100;
+  N = 100;
+  nomAlpha = 2*pi/3;
 
-  alpha = epgInversionFSE( t1, t2, T1, T2 );
+  alpha0 = pi/3;
+  phi0 = 0;
+  Ralpha0 = epgMakeR( alpha0, phi0 );
+  Q0m = sparse(3,N);  Q0m(3,1)=1;
+	Q0 = sparse( Ralpha0 * Q0m );
+  %Q0 = epgFseSim(t1,t2,T1,T2,nomAlpha,3,N);
 
+  figure; hold on;
+  for i=1:2
+    if i==1
+      alpha = nomAlpha * ones(J,1);
+    else
+      alpha = epgInversionFSE( t1, t2, T1, T2, J, nomAlpha, Q0 );
+    end
 
+    % Simulate EPG signal
+    allQs = zeros(J,3,N);
+    Q = Q0;
+    for j=1:J
+      Q = epgRelax( Q, t1, T1, T2 );
+      Q = epgGrad( Q, 1 );
+      Ralpha = epgMakeR( alpha(j), pi/2 );
+      Q = Ralpha * Q;
+      Q = epgRelax( Q, t2, T1, T2 );
+      Q = epgGrad( Q, 1 );
+      allQs(j,:,:) = Q;
+    end
+
+    sig = squeeze( allQs(:,1,1) );
+    plotnice( abs(sig) );
+  end
+  legend( 'Nominal', 'Adjusted' );
 end
+
+
+function Q = epgFseSim(t1,t2,T1,T2,alpha,J,N)
+  alpha0 = pi/2;
+  phi0 = 0;
+  Q = zeros(3,N);
+  Q(3,1) = 1;
+  Ralpha0 = epgMakeR( alpha0, phi0 );
+  Q = Ralpha0 * Q;
+  for j=1:J
+    Q = epgRelax( Q, t1, T1, T2 );
+    Q = epgGrad( Q, 1 );
+    Ralpha = epgMakeR( alpha, pi/2 );
+    Q = Ralpha * Q;
+    Q = epgRelax( Q, t2, T1, T2 );
+    Q = epgGrad( Q, 1 );
+  end
+end
+
 
